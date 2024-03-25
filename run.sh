5a2ca8992ec61740748897b620558305aca09432
Gary Sangwell <gary.sangwell@alces-software.com>
2024-03-25 10:55:56 +0000

    Only expose non-external ports on localhost.

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ docker run -d -it \
 	--mount type=bind,source=/etc/alces-dashboard/metrics/targets,target=/etc/victoria-metrics/targets \
 	alces-dashboard-metrics:latest \
 	--retentionPeriod=30d \
-	--httpListenAddr=:8428 \
+	--httpListenAddr=localhost:8428 \
 	--dedup.minScrapeInterval=60s \
 	--promscrape.config=/etc/victoria-metrics/prometheus.yml \
 	--promscrape.configCheckInterval=60s
@@ -37,7 +37,7 @@ docker run -d -it \
 	--volume metrics-downsampled-data:/victoria-metrics-data \
 	alces-dashboard-metrics:latest \
 	--retentionPeriod=730d \
-	--httpListenAddr=:8429 \
+	--httpListenAddr=localhost:8429 \
 	--dedup.minScrapeInterval=3600s \
 	--promscrape.configCheckInterval=60s
 
@@ -47,6 +47,7 @@ docker run -d -it \
 	--name alces-dashboard-downsampling \
 	--mount type=bind,source=/etc/alces-dashboard/downsampling/rules,target=/etc/vmalert/rules \
 	alces-dashboard-downsampling:latest \
+	--httpListenAddr=localhost:8880 \
 	--datasource.url=http://localhost:8428 \
 	--remoteWrite.url=http://localhost:8429 \
 	--remoteRead.url=http://localhost:8429 \
