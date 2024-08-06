2692544e8d7b74cd2a9104cfe8882d3e243a467d
Gary Sangwell <gary.sangwell@alces-software.com>
2024-08-06 09:51:41 +0100

    Add option to switch docker restart policy in run.sh.

--- run.sh
+++ run.sh
@@ -10,9 +10,15 @@ else
   IMAGE_SUFFIX=""
 fi
 
+if [[ "${EL7}" == "yes" ]] ; then
+  RESTART_POLICY="always"
+else
+  RESTART_POLICY="unless-stopped"
+fi
+
 docker run -d -it \
 	--network host \
-	--restart unless-stopped \
+	--restart ${RESTART_POLICY} \
 	--name alces-dashboard-grafana${IMAGE_SUFFIX} \
 	--volume grafana-data:/var/lib/grafana \
 	--mount type=bind,source=${DEPLOY_PATH}/grafana/custom.ini,target=/etc/grafana/grafana.ini \
@@ -24,7 +30,7 @@ docker run -d -it \
 
 docker run -d -it \
 	--network host \
-	--restart unless-stopped \
+	--restart ${RESTART_POLICY} \
 	--name alces-dashboard-proxy${IMAGE_SUFFIX} \
 	--mount type=bind,source=${DEPLOY_PATH}/certs,target=/etc/nginx/certs \
 	--mount type=bind,source=/var/lib/sss/pipes,target=/var/lib/sss/pipes \
@@ -34,7 +40,7 @@ docker run -d -it \
 if [[ -z "${DATA_PATH}" ]] ; then
 	docker run -d -it \
 	--network host \
-	--restart unless-stopped \
+	--restart ${RESTART_POLICY} \
 	--name alces-dashboard-metrics${IMAGE_SUFFIX} \
 	--volume metrics-data:/victoria-metrics-data \
 	--mount type=bind,source=${DEPLOY_PATH}/metrics/configs,target=/etc/victoria-metrics/configs \
@@ -49,7 +55,7 @@ if [[ -z "${DATA_PATH}" ]] ; then
 
 	docker run -d -it \
 	--network host \
-	--restart unless-stopped \
+	--restart ${RESTART_POLICY} \
 	--name alces-dashboard-metrics-downsampled${IMAGE_SUFFIX} \
 	--volume metrics-downsampled-data:/victoria-metrics-data \
 	--health-cmd='curl -s --fail http://localhost:8429/health || exit 1' \
@@ -64,7 +70,7 @@ else
 
 	docker run -d -it \
         --network host \
-        --restart unless-stopped \
+        --restart ${RESTART_POLICY} \
         --name alces-dashboard-metrics${IMAGE_SUFFIX} \
         --volume ${DATA_PATH}/metrics:/victoria-metrics-data \
         --mount type=bind,source=${DEPLOY_PATH}/metrics/configs,target=/etc/victoria-metrics/configs \
@@ -79,7 +85,7 @@ else
 
 	docker run -d -it \
         --network host \
-        --restart unless-stopped \
+        --restart ${RESTART_POLICY} \
         --name alces-dashboard-metrics-downsampled${IMAGE_SUFFIX} \
         --volume ${DATA_PATH}/metrics-downsampled:/victoria-metrics-data \
         --health-cmd='curl -s --fail http://localhost:8429/health || exit 1' \
@@ -92,7 +98,7 @@ fi
 
 docker run -d -it \
 	--network host \
-	--restart unless-stopped \
+	--restart ${RESTART_POLICY} \
 	--name alces-dashboard-downsampling${IMAGE_SUFFIX} \
 	--mount type=bind,source=${DEPLOY_PATH}/downsampling/rules,target=/etc/vmalert/rules \
 	--health-cmd='curl -s --fail http://localhost:8880/health || exit 1' \
@@ -106,11 +112,8 @@ docker run -d -it \
 
 docker run -it -d \
 	--network host \
-	--restart unless-stopped \
+	--restart ${RESTART_POLICY} \
 	--name alces-dashboard-power-exporter${IMAGE_SUFFIX} \
 	--mount type=bind,source=${DEPLOY_PATH}/power-exporter,target=/power-exporter/power-exporter/etc \
 	--health-cmd='curl -s --fail http://localhost:9106 || exit 1' \
 	alces-dashboard-power-exporter${IMAGE_SUFFIX}:latest
-
-
-
